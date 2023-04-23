dd5bdf48390687f0d46410832e01d03632a54755
vladhoang <ndsdarkwalker@gmail.com>
2023-04-23 22:18:49 +0700

    pj4-serverless-app

--- course-04/exercises/c4-client-master/src/config.ts
+++ course-04/exercises/c4-client-master/src/config.ts
@@ -2,7 +2,7 @@ const apiId = 'nhmk625tw0'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'test-endpoint.auth0.com',
-  clientId: 'A72UMiQQJzmHQRPpe1dHOyq3EfPvo3Hy',
+  domain: 'vladhoang.us.auth0.com',
+  clientId: 'hnyr2HH1LeO3DC2keapYB2Q6CpIn8jy9',
   callbackUrl: 'http://localhost:3000/callback'
 }
--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'aqnf40vvl8'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'vladhoang.us.auth0.com',
+  clientId: 'vkc1Cbh9lJjzoKsicdizyOpmisS6OZpv',
   callbackUrl: 'http://localhost:3000/callback'
 }
